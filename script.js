986a70c51647ad065a9e2a24fbaf3f8b6e6c33e0
Abibas12 <163030434+Abibas12@users.noreply.github.com>
2025-06-16 20:18:09 +0300

    Update script.js

--- script.js
+++ script.js
@@ -127,3 +127,10 @@ keys.forEach(button => {
               document.getElementById('slider').checked = true;
             }
         })();
+logo.addEventListener('mousemove', function(e) {
+    let dx = e.pageX - window.innerWidth / 2
+    let dy = e.pageY - window.innerHeight / 2
+    let angleX = 20 * dx / window.innerWidth / 2
+    let angleY = 20 * dy / window.innerHeight / 2
+    block.style.transform = `rotateX(${angleY}deg) rotateY(${angleX}deg)`
+})
